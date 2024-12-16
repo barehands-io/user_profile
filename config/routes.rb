ff69e6e9c70161258751090e8338104ddb4f13d1
Barehands <121155435+barehands-io@users.noreply.github.com>
2024-12-16 21:38:39 +0000

    Implement Users resource with CRUD actions and views; set root path to users index

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :users, only: [ :new, :create, :show, :index, :edit, :update ]
+  root "users#index"
 end
